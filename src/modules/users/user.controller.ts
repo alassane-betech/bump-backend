fb30a2c96f4c017efa3feffb312880a9287a94a2
vihoserge <vihoserge@gmail.com>
2024-03-08 17:34:02 +0000

    [VS] Add endpoint to validate email

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { UserService } from "./user.service";
 import { UserEntity } from "./entities/user.entity";
+import { UserService } from "./user.service";
 
 @Controller("users")
 export class UserController {
@@ -15,5 +15,10 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Get("validate-email")
+  async validateEmail(@Query("email") email: string): Promise<boolean> {
+    return !(await this.userService.isEmailExist(email));
   }
 }
