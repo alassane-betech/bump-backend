3d9594f788eeb9c4722f22d5ff8ec0c19b3fb4d1
vihoserge <vihoserge@gmail.com>
2024-03-12 18:26:58 +0000

    Fix: Add date validation for birthdate property in register dto

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsEnum, IsString } from "class-validator";
+import { IsDateString, IsEmail, IsEnum, IsString } from "class-validator";
 import { UserCategoryEnum } from "src/modules/users/enums/user-category.enum";
 
 export class RegisterDto {
@@ -24,7 +24,14 @@ export class RegisterDto {
   password: string;
 
   @ApiProperty()
-  @IsString()
+  @IsDateString(
+    { strict: true },
+    {
+      message: ({ property }) => {
+        return `${property} must be a valid date (Required format: YYYY-MM-DD)`;
+      }
+    }
+  )
   birthdate: string;
 
   @ApiProperty()
