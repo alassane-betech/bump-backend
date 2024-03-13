55205f9c1a9d7806e01e75a229ee2a66e34c6334
Seydina Gueye <dina3903@gmail.com>
2024-03-13 22:01:28 +0000

    bump:feat:exception-filter:add new custom http exception and implement HttpExceptionFilter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { UserModule } from "./modules/users/user.module";
 import { ORM_CONFIG } from "./database/ormconfig";
 import { AuthModule } from "./modules/auth/auth.module";
 import { FollowerModule } from "./modules/followers/follower.module";
+import { APP_FILTER } from "@nestjs/core";
+import { HttpExceptionFilter } from "./exceptions/http-exception.filter";
 
 @Module({
   imports: [TypeOrmModule.forRoot(ORM_CONFIG), UserModule, AuthModule, FollowerModule],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    }
+  ]
 })
 export class AppModule {}
